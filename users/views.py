62cb4ad31ecda8e19f5e6228234bc585ff63f7e4
jona <jona@jona.com>
2020-02-27 17:16:08 -0500

    login and logout system

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
@@ -9,11 +10,16 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-        	form.save()
-        	username=form.cleaned_data.get('username')
-        	messages.success(request,f"Account for {username} has been created successfully")
-        	return redirect('blog_home')
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, "Your account has been created, you can log in.")
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
