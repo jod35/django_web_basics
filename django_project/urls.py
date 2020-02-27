3f2b079c74ef069331107d1da3c6626bd11b44e2
jona <jona@jona.com>
2020-02-27 16:17:47 -0500

    user logins

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
+    path('register/',views.register,name='register'),
+
 ]
