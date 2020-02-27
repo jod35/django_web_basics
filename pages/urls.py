1bb5567bfb7eab17bf7fd1489d80e17cfbbe0594
jona <jona@jona.com>
2020-02-27 11:38:42 -0500

    first commit

--- pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.HomePage.as_view(),name='pages_home'),
-    path('about/',views.AboutPage.as_view(),name='pages_about')
-]
--- pages/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import TemplateView
-
-class HomePage(TemplateView):
-    template_name='pages/index.html'
-
-class AboutPage(TemplateView):
-    template_name='pages/about.html'
